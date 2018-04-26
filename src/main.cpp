a58165e056f0897994982c501acc4cfffa50881c
Simon Barker <simon.barker01@sap.com>
2018-04-26 21:38:11 +0100

    Added joystick test

--- src/main.cpp
+++ src/main.cpp
@@ -38,6 +38,7 @@ using namespace Menu;
 
 #define MENU_TEXT_SCALE 1
 #define MAX_DEPTH 4
+#define GRAY RGB565(32,32,32)
 
 MPU6050 mpu;
 SSD_13XX gfx(OLED_CS_PIN, OLED_DC_PIN, OLED_RST_PIN, OLED_MOSI_PIN, OLED_SCLK_PIN);
@@ -66,8 +67,8 @@ float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gra
 
 int test=55;
 int ledCount=0;
-int motorCtrl=HIGH;
-
+bool joystickTestMode = false;
+int lastx, lasty;
 result doAlert(eventMask e, prompt &item);
 
 using namespace Menu;
@@ -93,19 +94,35 @@ result doToggleMotorCtrl() {
   enableMotors(robotConfig.motorConfig.enabled);
   return proceed;
 }
+
 result doMotorStep() {
   setMotorStep(robotConfig.motorConfig.stepMode);
   return proceed;
 }
+result doJoystickTest() {
+
+  gfx.fillRect(0, 27, 191, 39,GRAY);
+  lastx=0;
+  lasty=0;
+  return proceed;
+}
+
 
 result doSaveConfig() {
   writeConfig(robotConfig);
   return proceed;
 }
 
+
 TOGGLE(robotConfig.motorConfig.enabled,setMotor,"Motor Control: ",doNothing,noEvent,noStyle//,doExit,enterEvent,noStyle
   ,VALUE("Off", false, doToggleMotorCtrl, enterEvent)
   ,VALUE("On", true, doToggleMotorCtrl, enterEvent)
+);
+
+
+TOGGLE(joystickTestMode,joystickTest,"Joystick test: ",doNothing,noEvent,noStyle//,doExit,enterEvent,noStyle
+  ,VALUE("Off", false, doJoystickTest, enterEvent)
+  ,VALUE("On", true, doJoystickTest, enterEvent)
 );
 
 int selTest=0;
@@ -134,15 +151,16 @@ public:
   }
 };
 
-MENU(subMenu,"Sub-Menu",doNothing,noEvent,noStyle
-  ,altOP(altPrompt,"",doNothing,noEvent)
-  ,OP("Op",doNothing,noEvent)
-  ,EXIT("<Back")
-);
-
 MENU(motorMenu,"Motor Cfg",doNothing,noEvent,noStyle
   ,SUBMENU(setMotor)
   ,SUBMENU(chooseMotorModeMenu)
+  ,EXIT("<Back")
+);
+
+
+
+MENU(joystickMenu,"Joystick test",doNothing,noEvent,noStyle
+  ,SUBMENU(joystickTest)
   ,EXIT("<Back")
 );
 
@@ -152,9 +170,9 @@ char buf1[]="0x11";
 
 MENU(mainMenu,"Robot Control Menu",doNothing,noEvent,wrapStyle
   ,SUBMENU(motorMenu)
-  //,SUBMENU(setMotor)
   ,SUBMENU(selMenu)
-//  ,SUBMENU(chooseMotorModeMenu)
+  //,SUBMENU(mpuMenu)
+  ,SUBMENU(joystickMenu)
   ,OP("Save Configuration",doSaveConfig,enterEvent)
   ,FIELD(test,"Test","%",0,100,10,1,doNothing,noEvent,wrapStyle)
   //,EDIT("Hex",buf1,hexNr,doNothing,noEvent,noStyle)
@@ -166,7 +184,7 @@ MENU(mainMenu,"Robot Control Menu",doNothing,noEvent,wrapStyle
 //  {{disabled normal,disabled selected},{enabled normal,enabled selected, enabled editing}}
 //monochromatic color table
 
-#define GRAY RGB565(32,32,32)
+
 
 const colorDef<uint16_t> colors[] MEMMODE={
   {{BLACK,BLACK},{GRAY,BLUE,BLUE}},//bgColor
@@ -182,7 +200,7 @@ MENU_INPUTS(in,&strIn);
 
 
 MENU_OUTPUTS(out,MAX_DEPTH
-  ,SSD1331_OUT(gfx,colors,6*MENU_TEXT_SCALE,9*MENU_TEXT_SCALE,{0,0,25,4})
+  ,SSD1331_OUT(gfx,colors,6*MENU_TEXT_SCALE,9*MENU_TEXT_SCALE,{0,0,25,7})
   //,SSD1331_OUT(gfx,colors,6*MENU_TEXT_SCALE,9*MENU_TEXT_SCALE,{0,0,20,6}, {10,0,10,6})
   //  ,SSD1331_OUT(gfx,colors,6*MENU_TEXT_SCALE,9*MENU_TEXT_SCALE,{0,0,14,8},{14,0,14,8})
   ,SERIAL_OUT(Serial)
@@ -385,6 +403,31 @@ void fadeLed() {
 void loop() {
     checkForNavInput(strIn);
     nav.poll();
-    fadeLed();
-    delay(1);
+    if (joystickTestMode) {
+      int y = goble.readJoystickX();
+      int x = goble.readJoystickY();
+      if ((lastx != x) ||
+          (lasty != y) ) {
+            gfx.fillRect(0, 27, 191, 39,GRAY);
+            gfx.setCursor(0,35);
+            gfx.setTextColor(WHITE);
+            gfx.print("X: ");
+            gfx.setTextColor(YELLOW);
+            gfx.print(x);
+
+            gfx.setCursor(0,45);
+            gfx.setTextColor(WHITE);
+            gfx.print("Y: ");
+            gfx.setTextColor(YELLOW);
+            gfx.print(y);
+
+            gfx.drawCircle(58, 43, 15, WHITE);
+            gfx.fillCircle((x>>4) + 50, ((255-y)>>4)+ 35, 5, YELLOW);
+
+            lastx = x;
+            lasty = y;
+      }
+    }
+    //fadeLed();
+    delay(5);
 }
